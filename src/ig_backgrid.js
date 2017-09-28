5c084f9229d9b49ba39bfbd72e18fda4b6db83d5
Amenadiel <amenadiel@gmail.com>
2017-09-28 09:09:25 -0300

    testing with pure karma, removing grunt, removing backbone sources

---
+++ src/ig_backgrid.js
@@ -0,0 +1,21 @@
+import {
+  PageableCollection
+} from './backgrid_extensions/backbone.paginator.js';
+
+import {
+  Backgrid
+} from 'backgrid';
+
+import {
+  PageHandle,
+  Paginator
+} from './backgrid_extensions/backgrid-paginator.js';
+
+import {
+  SizeAbleColumns
+} from './backgrid_extensions/backgrid-sizeable-columns.js';
+
+export {
+  Backgrid
+};
+export default Backgrid;
